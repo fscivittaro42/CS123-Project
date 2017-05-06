b97b1361008cc046d8c323fe1ba30d1c18cad6f8
Salman <sarif6@uchicago.edu>
2017-05-06 18:29:54 -0500

    shell script for nyc data

---
+++ wget_NYC.sh
@@ -0,0 +1,17 @@
+
+
+mkdir -p data/yellow/yearmo
+mkdir -p data/yellow/samples 
+cd data/yellow
+
+for YEAR in 2016
+do
+	for MONTH in 01 02 03 04 05 06 07 08 09 10 11 12
+	do 
+		wget -O yearmo/yellow_tripdata_$YEAR_$MONTH.csv \
+		https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_$YEAR-$MONTH.csv
+		subsample -r -n 2000 yearmo/yellow_tripdata_$YEAR_$MONTH.csv \
+		> samples/yellow_sample_$YEAR_$MONTH.csv
+	done 
+done 
+
