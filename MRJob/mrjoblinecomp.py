420d0ac8b47caba1e6546a456cd17426a989c547
Andrew Chuang <chuangspokane@gmail.com>
2017-05-17 21:46:53 -0500

    Preliminary MRJob Files
    Three MRJob files that I have worked on. Mostly contain code that finds
    basic summary statistics rather than anything complicated.

---
+++ MRJob/mrjoblinecomp.py
@@ -0,0 +1,24 @@
+# CMSC 123
+# Comparison between Line 1 and all other lines
+# Average tip as a proportion of fare
+
+
+from mrjob.job import MRJob
+
+class TaxiAvgFare(MRJob):
+
+    def mapper(self, _, line):
+    	column = line.split(',')
+    	fare = float(column[12])
+    	tip = float(column[15])
+    	if fare != 0 and tip != 0:
+    	    proportion = float(tip)/float(fare)
+    	    yield None, proportion
+
+    def reducer(self, _, proportion):
+    	proportion_list = list(proportion)
+    	yield None, sum(proportion_list)/len(proportion_list)
+
+
+if __name__ == '__main__':
+    TaxiAvgFare.run()
---
+++ MRJob/mrjobtaxi.py
@@ -0,0 +1,23 @@
+# CMSC 123
+# Test MRJob code for NYC Yellow Taxi Data
+#
+
+
+from mrjob.job import MRJob
+
+
+class TaxiRiderMax(MRJob):
+
+    def mapper(self, _, line):
+        column_var = line.split(',')
+        rider_count = column_var[4]
+        yield None, max(rider_count)
+
+    def combiner(self, _, count):
+        yield None, max(count)
+
+    def reducer(self, _, count):
+        yield None, max(count)
+
+if __name__ == '__main__':
+    TaxiRiderMax.run()
