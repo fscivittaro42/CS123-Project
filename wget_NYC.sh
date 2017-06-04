bd34c680f3befb7c5a215608f344b701b48a3764
Salman <sarif6@uchicago.edu>
2017-06-03 21:14:16 -0500

    header for wget_NYC

--- wget_NYC.sh
+++ wget_NYC.sh
@@ -1,10 +1,17 @@
+# Arif-Chuang-Scivittaro
+# CMSC 123 Spring 2017
+# Chicago Taxi Data Project
+#
+# Preliminary shell script to download and make samples
+# of NYC Taxi Cab data. Unused in final project. 
+
 pip3 install subsample 
 
 mkdir -p data/yellow/yearmo
 mkdir -p data/yellow/samples 
 cd data/yellow
 
-for YEAR in 2016
+for YEAR in 2016 2015 2014 2013 2012 
 do
 	for MONTH in 01 02 03 04 05 06 07 08 09 10 11 12
 	do 
