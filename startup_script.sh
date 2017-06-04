0866cd81b2af152305fdb8e9642c5e8f81dda505
Salman <sarif6@uchicago.edu>
2017-06-03 21:17:05 -0500

    header for startup script

--- startup_script.sh
+++ startup_script.sh
@@ -1,3 +1,11 @@
+# Arif-Chuang-Scivittaro
+# CMSC 123 Spring 2017
+# Chicago Taxi Data Project
+#
+# Shell script to run in Google Cloud Platform VM upon creation
+# to install some basic items. 
+
+
 sudo apt-get -y install ipython3 
 sudo apt-get -y install python3-pip
 sudo apt-get -y install git 
@@ -6,5 +14,4 @@ sudo pip3 install mrjob
 sudo pip3 install subsample 
 
 
-
-gcloud init
+# gcloud init
