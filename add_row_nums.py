36db0a1111a5f6406fab69f6a814aec2f4aa6f62
Salman <sarif6@uchicago.edu>
2017-06-04 01:13:52 -0500

    rownums

---
+++ add_row_nums.py
@@ -0,0 +1,33 @@
+# Arif-Chuang-Scivittaro
+# CMSC 123 Spring 2017
+# Chicago Taxi Data Project
+#
+# Quick script to add row numbers to the front of each row
+# in a data csv file. 
+
+import csv
+import sys
+
+def add_row_nums(file):
+
+	'''
+	file: must be full path to file. 
+	'''
+
+	i = 0
+	assert file[-3:] == 'csv'
+
+	with open(file, 'r') as f, open(file[0:-4] + '_nums.csv', 'w') as out:
+		
+		w = csv.writer(out)
+		r = csv.reader(f)
+		for line in r:
+			w.writerow([i] + [col for col in line])
+			i+=1
+
+
+
+if __name__ == '__main__':
+
+	file = sys.argv[1]
+	add_row_nums(file)
