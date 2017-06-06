0c9a5e0f8c1a4555d68e958dc4eeb458ca5b829c
Salman <sarif6@uchicago.edu>
2017-06-05 23:05:57 -0500

    5k

--- generate_squares/pairs_squares_dataproc.py
+++ generate_squares/pairs_squares_dataproc.py
@@ -40,7 +40,7 @@ class MRCompare(MRJob):
         A method that initializes a copy of the data CSV file so that pairs of
         trips can be generated
         '''
-        self.f = open(cwd + '/50k.csv', 'rb')
+        self.f = open(cwd + '/5k.csv', 'rb')
         next(self.f)
         self.bytes = self.f.tell()
         self.length = self.f.seek(0,2)
